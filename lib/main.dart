bd8becc46c9154d747d86e79279cc2844d7e6e5f
Trevor Apple <trappleco@gmail.com>
2022-02-03 20:01:56 -0800

    can now generate a random 5-letter word

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -20,11 +21,13 @@ class _LetterGridState extends State<LetterGrid> {
   var guesses = <String>[];
   int startGuess = 0;
   final wordLength = 5;
+  String word = '';
 
   final TextEditingController _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    loadWords();
     const charBoxDecoration = BoxDecoration(
       color: Color(0xFF5F5F5F),
     );
@@ -37,30 +40,33 @@ class _LetterGridState extends State<LetterGrid> {
     var gridViewChildren = <Widget>[];
 
     for (var i = 0; i < 30; i++) {
-      gridViewChildren.add(Container(
-        child: Align(
-            alignment: Alignment.center,
-            child: Text(guesses[i], style: charStyle)),
-        decoration: charBoxDecoration,
-      ));
+      gridViewChildren.add(
+        Container(
+          child: Align(
+              alignment: Alignment.center,
+              child: Text(guesses[i], style: charStyle)),
+          decoration: charBoxDecoration,
+        ),
+      );
     }
 
-    gridViewChildren.add(Visibility(
+    gridViewChildren.add(
+      Visibility(
         visible: false,
         maintainState: true,
         maintainAnimation: true,
         maintainSize: true,
         maintainInteractivity: true,
         child: TextField(
-            controller: _controller,
-            autofocus: true,
-            autocorrect: false,
-            enableSuggestions: false,
-            keyboardType: TextInputType.visiblePassword,
-            maxLength: 5,
-            onChanged: (value) {
-              debugPrint('${value}, ${startGuess}, ${guesses}');
-              setState(() {
+          controller: _controller,
+          autofocus: true,
+          autocorrect: false,
+          enableSuggestions: false,
+          keyboardType: TextInputType.visiblePassword,
+          maxLength: 5,
+          onChanged: (value) {
+            setState(
+              () {
                 if (value.length > 0) {
                   for (var i = startGuess; i < startGuess + value.length; i++) {
                     guesses[i] = value[i % wordLength];
@@ -75,29 +81,45 @@ class _LetterGridState extends State<LetterGrid> {
                     guesses[i] = '';
                   }
                 }
-              });
-            },
-            onSubmitted: (value) {
-              setState(() {
-                debugPrint('resetting text field?');
-                startGuess += wordLength;
-                // TODO: handle reaching the end
-                _controller.clear();
-              });
-            },
-            onEditingComplete: () {})));
+              },
+            );
+          },
+          onSubmitted: (value) {
+            setState(
+              () {
+                if (startGuess < 25) {
+                  startGuess += wordLength;
+                  // TODO: handle reaching the end
+                  _controller.clear();
+                } else if (startGuess == 25) {}
+              },
+            );
+          },
+          onEditingComplete: () {},
+        ),
+      ),
+    );
 
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Wordle Clone'),
-        ),
-        backgroundColor: Color(0xFF000000),
-        body: GridView.count(
-            crossAxisCount: 5,
-            padding: const EdgeInsets.all(20),
-            crossAxisSpacing: 5,
-            mainAxisSpacing: 5,
-            children: gridViewChildren));
+      appBar: AppBar(
+        title: const Text('Wordle Clone'),
+      ),
+      backgroundColor: Color(0xFF000000),
+      body: GridView.count(
+          crossAxisCount: 5,
+          padding: const EdgeInsets.all(20),
+          crossAxisSpacing: 5,
+          mainAxisSpacing: 5,
+          children: gridViewChildren),
+    );
+  }
+
+  void loadWords() async {
+    String rawWords = await rootBundle.loadString('assets/words.txt');
+    final words = rawWords.split('\n');
+    final wordSelector = Random(1643945711103346);
+    this.word = words[wordSelector.nextInt(words.length)];
+    debugPrint(this.word);
   }
 }
 
@@ -106,85 +128,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Startup name generator', home: LetterGrid());
+    return MaterialApp(title: 'Wordle Clone', home: LetterGrid());
   }
 }
-
-// import 'package:flutter/material.dart';
-
-// void main() => runApp(const MyApp());
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       title: 'Text Field Focus',
-//       home: MyCustomForm(),
-//     );
-//   }
-// }
-
-// // Define a custom Form widget.
-// class MyCustomForm extends StatefulWidget {
-//   const MyCustomForm({Key? key}) : super(key: key);
-
-//   @override
-//   _MyCustomFormState createState() => _MyCustomFormState();
-// }
-
-// // Define a corresponding State class.
-// // This class holds data related to the form.
-// class _MyCustomFormState extends State<MyCustomForm> {
-//   // Define the focus node. To manage the lifecycle, create the FocusNode in
-//   // the initState method, and clean it up in the dispose method.
-//   late FocusNode myFocusNode;
-
-//   @override
-//   void initState() {
-//     super.initState();
-
-//     myFocusNode = FocusNode();
-//   }
-
-//   @override
-//   void dispose() {
-//     // Clean up the focus node when the Form is disposed.
-//     myFocusNode.dispose();
-
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Text Field Focus'),
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: Column(
-//           children: [
-//             // The first text field is focused on as soon as the app starts.
-//             const TextField(
-//               autofocus: true,
-//             ),
-//             // The second text field is focused on when a user taps the
-//             // FloatingActionButton.
-//             TextField(
-//               focusNode: myFocusNode,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         // When the button is pressed,
-//         // give focus to the text field using myFocusNode.
-//         onPressed: () => myFocusNode.requestFocus(),
-//         tooltip: 'Focus Second Text Field',
-//         child: const Icon(Icons.edit),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
